b9fa63c76f53fa6c16d25d779058e71b1c6e4399
m3jd3n <129152592+m3jd3n@users.noreply.github.com>
2024-03-22 22:28:24 +0000

    Homework 4 - online shop
    Added  basic functions to  Shop, Basket and Product classes like adding and removing item, changing and displaying status, listing items

---
+++ Online_shop/shop.py
@@ -0,0 +1,53 @@
+import shop_structure
+
+def add_to_basket(shop, basket):
+    input_product = input("Enter product name to add to basket: ");
+    product_obj = shop.get_product(input_product)
+    print(product_obj)
+
+    if product_obj:
+        print(f"Adding product: {product_obj.name} to basket...")
+        basket.add(product_obj)
+    else:
+        print("Product not found")
+
+def create_order(shop, basket, order_database):
+    if not basket.is_empty():
+        print("Creating order...")
+        order = shop_structure.Order(basket)
+        order_database.add_order(order)
+    else:
+        print("Basket is empty")
+        return
+
+
+def main():
+    shop = shop_structure.Shop()
+    shop.add_product(shop_structure.Product("Apple", 10))
+    shop.add_product(shop_structure.Product("Banana", 5))
+    shop.add_product(shop_structure.Product("Orange", 8))
+    shop.add_product(shop_structure.Product("Pineapple", 15))
+
+    shop.list_products()
+
+    basket = shop_structure.Basket()
+
+    add_to_basket(shop, basket)
+    add_to_basket(shop, basket)
+
+    print(basket)
+
+    order_database = shop_structure.OrderDatabase()
+
+    create_order(shop, basket, order_database)
+
+    print(order_database)
+
+    print("Listing orders:")
+    order_database.list_orders()
+
+    print("Listing order items fro order database:")
+    order_database.orders[0].basket.basket_items_list()
+
+if __name__ == "__main__":
+    main()
--- Online_shop/shop_structure.py
+++ Online_shop/shop_structure.py
@@ -1,3 +1,26 @@
+
+class Shop:
+    def __init__(self):
+        self.products = []
+
+    def add_product(self, product):
+        self.products.append(product)
+
+    def list_products(self):
+        print("Listing products:")
+        for product in self.products:
+            print(product)
+
+    def get_product(self, product_name):
+        for product in self.products:
+            if product.name.upper() == product_name.upper():
+                return product
+        return None
+
+    def __str__(self):
+        return f"Shop with {len(self.products)} products"
+
+
 class Basket:
     def __init__(self):
         self.items = []
@@ -7,21 +30,31 @@ class Basket:
 
     def remove(self, product):
         self.items.remove(product)
+
+    def is_empty(self):
+        return len(self.items) == 0
         
     def total(self):
         return sum(product.price for product in self.items)
 
     def __str__(self):
         return f"Basket with {len(self.items)} items"
+
+    def basket_items_list(self):
+        print("Basket items:")
+        for item in self.items:
+            print(item)
+
     
 class Product:
-    def __init__(self, name, price, quantity = 0):
+    def __init__(self, name, price):
         self.name = name
         self.price = price
-        self.quantity = quantity
+
 
     def __str__(self):
-        return f"{self.name} price: {self.price} quantity: {self.quantity}"
+        return f"{self.name} price: {self.price}"
+
 
 class Order:
 
@@ -32,7 +65,15 @@ class Order:
         __order_status_states = ["new", "paid", "shipped", "delivered", "cancelled"]
 
     def __str__(self):
-        return f"Order number {self.order_id} with {len(self.basket.items)} items for total of {self.basket.total()}"
+        return f"Order number {self.order_id} with {len(self.basket.items)} items for total of {self.basket.total()} and status: {self.status}"
+
+    def get_status(self):
+        return self.status
+
+    def order_items_list(self):
+        print("Order items:")
+        self.basket.basket_items_list()
+
 
     def pay(self):
         self.status = "paid"
@@ -49,7 +90,6 @@ class Order:
 class OrderDatabase:
     def __init__(self):
         self.orders = []
-        self.order= order
         self.order_id = 0
 
     def add_order(self, order):
@@ -62,6 +102,15 @@ class OrderDatabase:
             if order.order_id == order_id:
                 return order
         return None
+
+    def list_orders(self):
+        if not self.orders:
+            print("No orders found")
+            return
+        else:
+            print("Listing orders:")
+            for order in self.orders:
+                print(order)
 
     def __str__(self):
         return f"Order database with {len(self.orders)} orders"
